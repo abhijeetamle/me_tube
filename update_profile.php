d60656eb4ca91283198d81e9cca0c9aca5eb5224
AbhijeetAmle <abhijeet.amle@gmail.com>
2020-04-13 21:12:15 -0400

    update profile added

--- update_profile.php
+++ update_profile.php
@@ -67,67 +67,119 @@ body {font-family: Arial, Helvetica, sans-serif;}
   cursor: pointer;
 }
 </style>
-</head>
+
+<?php
+
+function upPassword( $string ) { 
+
+    $string = sha1($string);
+    echo '<script>alert("inside php.")</script>';
+} 
+
+?>
 
 </head>
 
-<body> <!--  onclick="openTab('loginContainer')" -->
 
+
+<body> <!--  onclick="Update" -->
+<?php
+
+    // password update
+    if (isset($_POST['updatePass'])) {
+
+        if($_POST['pwd'] != $_POST['confirm_pwd']){
+              echo '<script>alert("Passwords must match")</script>';
+        }
+        else{
+            $updateP = "UPDATE USER_ACCOUNT SET password = '" .sha1($_POST['pwd']). "' WHERE email_id = '" .$_SESSION['username']."'";
+            if (mysqli_query($mysqli, $updateP)) {
+    
+                echo '<script>alert("Password updated successfully.")</script>';
+            } 
+            else {
+                echo '<script>alert("Error occured while updating your password. Please try again.")</script>';
+            }
+        }
+    }
+
+
+    // profile update 
+    if (isset($_POST['updateButton'])) {
+
+     //   echo '<script>alert("update button clicked.")</script>';
+        
+        $updateSql = "UPDATE USER_ACCOUNT SET first_name = '" .$_POST['firstName']. "', last_name = '" .$_POST['lastName']. "', 
+        gender = '" .$_POST['gender']."', dob = '" .$_POST['dob']. "' WHERE email_id = '" .$_SESSION['username']."'";
+        if (mysqli_query($mysqli, $updateSql)) {
+       
+            $_SESSION['firstname'] = $_POST["firstName"];
+        
+            echo '<script>alert("Account updated successfully.")</script>';
+            echo '<script>location.href="home.php"</script>';
+        } 
+        else {
+            echo '<script>alert("Error occured while updating your account. Please try again.")</script>';
+        }
+    
+    }
+    //close_db_connection();
+?>
+
+
+
+    <!-- The Modal -->
+  <form method="post">
+    <div id="myModal" class="modal">
+      <!-- Modal content -->
+      <div class="modal-content">
+        <span class="close">&times;</span>
+        <div class="form-popup" id="myForm">
+          <form class="form-container">
+            <h3><center>Update password</center></h3>
+            <label for="pwd">New Password &nbsp;&nbsp;&nbsp;&nbsp;</label>
+            <input type="password" class="form-control" id="pwd" name="pwd" minlength="8" required>
+              <br>
+              <br>
+            <label for="confirm_pwd">Confirm password</label>
+            <input type="password" class="form-control" id="confirm_pwd" name="confirm_pwd" minlength="8" required>
+            <br>
+            <br>
+            <button type="button" class="btn cancel" onclick="closeForm()">Cancel</button>
+            <a>&nbsp;&nbsp;</a>
+            <button type="submit" class="btn" name="updatePass">Update Password</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </form>
 
 <p1><center>Update profile information</center></p1>
 
-  <!-- This is registration container -->
-  <div class="container" name="regContainer" id="regContainer" style="width:50%;">
-    <form method="post"> <!-- onsubmit="location.href='home.php'; return false;" -->
-      <div class="form-group">
-        <label for="email">Email address</label>
-        <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" value="<?php echo $email; ?>" disabled="disabled" required>
-        <small id="emailHelp" class="form-text text-muted">E-mail id cannot be updated.</small>
-      
-      <br>
-      <!-- Trigger/Open The Modal -->
-<button id="myBtn">Update Password</button>
-
-<!-- The Modal -->
-<div id="myModal" class="modal">
-
-  <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close">&times;</span>
-    <div class="form-popup" id="myForm">
-  <form class="form-container">
-    <h3><center>Update password</center></h3>
-
-
-    <label for="pwd">New Password &nbsp;&nbsp;&nbsp;&nbsp;</label>
-    <input type="password" class="form-control" id="pwd" name="pwd" minlength="8" required>
-      <br>
-      <br>
-    <label for="confirm_pwd">Confirm password</label>
-    <input type="password" class="form-control" id="confirm_pwd" name="confirm_pwd" minlength="8" required>
-    <br>
+  <!-- This is update profile container  -->
+   
+<div class="container" name="updateCont" id="updateCont" style="width:50%;">
+<form method="post">
+    <label for="email">Email address</label>
+    <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" value="<?php echo $email; ?>" disabled="disabled" required>
+    <small id="emailHelp" class="form-text text-muted">E-mail id cannot be updated.</small>
     <br>
 
-    <button type="button" class="btn cancel" onclick="closeForm()">Cancel</button>
-    <a>&nbsp;&nbsp;</a>
-    <button type="submit" class="btn">Update Password</button>
-    
-  </form>
-</div>
-  </div>
+          <!-- Trigger/Open The Modal -->
+    <button id="myBtn">Update Password</button>
 
-</div>
+    <br>
+
       
-      
-      <div class="form-group">
+    <div class="form-group">
         <label for="firstName">First name</label>
         <input type="text" class="form-control" id="firstName" name="firstName" value="<?php echo $firstname; ?>" required>
-      </div>
-      <div class="form-group">
+    </div>
+    <div class="form-group">
         <label for="lastName">Last name</label>
         <input type="text" class="form-control" id="lastName" name="lastName" value="<?php echo $lastname; ?>" required>
-      </div>
-      <div class="form-group">
+    </div>
+    <div class="form-group">
         <label for="gender">Gender</label><br>
         <input type="radio" id="male" name="gender" value="M" <?php echo ($gender=='M')?'checked':'' ?> required>
         <label for="male">Male</label><br>
@@ -135,18 +187,21 @@ body {font-family: Arial, Helvetica, sans-serif;}
         <label for="female">Female</label><br>
         <input type="radio" id="other" name="gender" value="O" <?php echo ($gender=='O')?'checked':'' ?>>
         <label for="other">Other</label>
-      </div>
-      <div class="form-group">
+    </div>
+    <div class="form-group">
         <label for="dob">Date of Birth</label>
         <input type="date" class="form-control" id="dob" name="dob" value="<?php echo $dob; ?>" required>
-      </div>
-      <div class="form-group form-check">
+    </div>
+    <div class="form-group form-check">
         <input type="checkbox" class="form-check-input" id="exampleCheck1" required>
         <label class="form-check-label" for="exampleCheck1">I confirm that above information is correct.</label>
-      </div>
-      <button type="submit" class="btn btn-primary" name="regButton">Update</button>
-    </form>
-  </div>
+    </div>
+        <button type="button" class="btn cancel"  onclick="window.location.href = 'home.php';">Cancel</button>
+        <button type="submit" class="btn btn-primary" name="updateButton">Update</button>
+</form>
+</div>
+
+
 
 
 <script>
@@ -154,6 +209,7 @@ body {font-family: Arial, Helvetica, sans-serif;}
 function closeForm() {
   modal.style.display = "none";
 }
+
 // Get the modal
 var modal = document.getElementById("myModal");
 
